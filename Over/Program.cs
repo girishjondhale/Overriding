f17191a8316f86951feb045aae67ef574bb6da8f
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 17:00:28 -0700

    Add project files.

---
+++ Over/Program.cs
@@ -0,0 +1,20 @@
+﻿using Static;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Over
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(5, 8);
+            r.CalculateArea();
+            Console.WriteLine(r.ToString());
+
+        }
+    }
+}
